61084fb74fa79b0788da1dcb237bce53b50b87db
ottwil01 <ottwil01@gmail.com>
2022-11-12 17:30:39 +1100

    CRUD template

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
+from db import db, ma
+from controllers.cards_controller import cards_bp
+import os 
 
 def create_app():
     app = Flask(__name__)
     
+    @app.errorhandler(404)
+    def not_found(err):
+        return {'error' : str(err)}, 404 
+    
+    app.config['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+     
+    db.init_app(app)
+    ma.init_app(app)
     
-    db = SQLAlchemy(app)
+    app.register_blueprint(cards_bp)
     
-    @app.route('/')
-    def index():
-        return 'hello' 
     return app
